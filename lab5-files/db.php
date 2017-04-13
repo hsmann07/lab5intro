e6fe7f73ced487b02dd5fd65193dfdc02e8084d8
Harjot Singh Mann <hsmann07@gmail.com>
2017-04-13 17:33:03 -0400

    Add files via upload

---
+++ lab5-files/db.php
@@ -0,0 +1,7 @@
+<?php
+
+//connect to db
+$conn = new PDO('mysql:host=sql.computerstudi.es;dbname=gc200354653','gc200354653','f^e6v2Hg');
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+?>
---
+++ lab5-files/search.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+
+<?php
+
+require_once('header.php');
+
+?>
+
+<form method="post" action="search-results.php">
+<div>
+	<label>Keywords</label>
+	<input name="keywords" />
+</div>
+<button>Search</button>
+</form>
+
+</body>
+</html>
